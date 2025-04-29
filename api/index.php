6e3e3eca8899acc73d903a49ca96606c502a9ec0
joker <web_joker@outlook.com>
2025-04-29 13:29:25 +0800

    back：代码回滚

--- api/index.php
+++ api/index.php
@@ -1,11 +1,4 @@
 <?php
-// 修复路径解析问题
-$requestUri = $_SERVER['REQUEST_URI'] ?? '/';
-$path = parse_url($requestUri, PHP_URL_PATH);
-$file = __DIR__ . '/..' . $path;
-
-if (file_exists($file) && !is_dir($file)) {
-    return false;
-} else {
-    require_once __DIR__ . '/../index.php';
-}
+$file = __DIR__ . '/..' . $_SERVER["PHP_SELF"];
+if (file_exists($file)) return false;
+else require_once __DIR__ . '/../index.php';
